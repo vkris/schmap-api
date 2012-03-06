ee8ffbd4603154861be6dd0ce0f71c04a2c39ab8
vkris <vivekrishna@gmail.com>
2012-03-05 20:18:50 -0500

    logger def as a function

--- schmap_api/__init__.py
+++ schmap_api/__init__.py
@@ -2,19 +2,23 @@ import os, sys
 sys.path.append(".")
 import logging
 
-format_string = "%(asctime)s %(name)s [%(levelname)s]:%(message)s"
-level = 'INFO'
-logger = logging.getLogger('schmap_api')
-logger.setLevel(level)
-#lhandler = logging.FileHandler("schmap_api.log")
-lhandler = logging.StreamHandler()
-lhandler.setLevel(level)
-fm = logging.Formatter(format_string)
-lhandler.setFormatter(fm)
-logger.addHandler(lhandler)
-log = logger
+
+def getLog():
+    format_string = "%(asctime)s %(name)s [%(levelname)s]:%(message)s"
+    level = 'INFO'
+    logger = logging.getLogger('schmap_api')
+    logger.setLevel(level)
+    #lhandler = logging.FileHandler("schmap_api.log")
+    lhandler = logging.StreamHandler()
+    lhandler.setLevel(level)
+    fm = logging.Formatter(format_string)
+    lhandler.setFormatter(fm)
+    logger.addHandler(lhandler)
+    return logger
 
 def client(username, password):
     from schmap_api.client_api import SchmapAPIClient
     return SchmapAPIClient(username, password)
+
+log = getLog()
 
