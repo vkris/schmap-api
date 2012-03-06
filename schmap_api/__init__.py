ed9eda22ae2ebc5386f543ffef288f17f6a13780
vkris <vivekrishna@gmail.com>
2012-03-05 19:40:13 -0500

    Use stream handler and include a new method

--- schmap_api/__init__.py
+++ schmap_api/__init__.py
@@ -6,9 +6,15 @@ format_string = "%(asctime)s %(name)s [%(levelname)s]:%(message)s"
 level = 'INFO'
 logger = logging.getLogger('schmap_api')
 logger.setLevel(level)
-lhandler = logging.FileHandler("schmap_api.log")
+#lhandler = logging.FileHandler("schmap_api.log")
+lhandler = logging.StreamHandler()
 lhandler.setLevel(level)
 fm = logging.Formatter(format_string)
 lhandler.setFormatter(fm)
 logger.addHandler(lhandler)
 log = logger
+
+def client(username, password):
+    from schmap_api.client_api import SchmapAPIClient
+    return SchmapAPIClient(username, password)
+
