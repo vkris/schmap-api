85878f5dfa1f7885f950e3223ff55b80d98aaa0b
vkris <vivekrishna@gmail.com>
2012-07-10 12:42:45 +0530

    password as arugment

--- examples/get_dataset.sh
+++ examples/get_dataset.sh
@@ -1,5 +1,8 @@
 # filename base, request id as parameters
+
+# The first request wiht no cursor argument
+bash -c "curl -o toyota-followers-chunk-0.json --user gensent:eb6j89kpbrtryy 'https://www.schmap.it/api/social_analysis/get_dataset?request_id=527'"
 for ((i=5000;i<=100000;i+=5000));
 do
-    bash -c "curl -o $1-chunk-$i.json --user username:password 'https://www.schmap.it/api/social_analysis/get_dataset?request_id=$2&cursor=$i'"
+    bash -c "curl -o $1-chunk-$i.json --$3:$4 'https://www.schmap.it/api/social_analysis/get_dataset?request_id=$2&cursor=$i'"
 done
